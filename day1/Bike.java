03987e263c72c57402fe88b78c7e345beebc05fa
kowsika2306 <139855788+kowsika2306@users.noreply.github.com>
2023-07-18 13:43:46 +0530

    Add files via upload
    day1 assignments

---
+++ day1/Bike.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int price=30000;
+        String brandname = "hero honda";
+        char logo = 'h';
+        float fuelcapacity = 5f;
+        long contactnumber = 78965412336l;
+        
+        System.out.println("price");
+        System.out.println("fuelcapacity is :" + fuelcapacity);
+        System.out.println("the owner of contact number is :" + contactnumber);
+	}
+
+}
---
+++ day1/FireFox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version=100.2f;
+		String name="firefox";
+		boolean visible=true;
+		int year=1993;
+		char logo ='m';
+		
+		System.out.println(version);
+		System.out.println(name);
+		System.out.println(visible);
+		System.out.println(year);
+		System.out.println(logo);
+		
+		
+		
+	}
+
+}
